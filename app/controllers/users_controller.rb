1825c48fe2675bb6e81dbef5f0bb9a52bfea4b0a
Md Hefzul Bari <ratul14april@gmail.com>
2022-02-08 20:12:34 -0600

    Feature:Spec product-details test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,19 @@
 class UsersController < ApplicationController
 
   def new
-    # @existing_emails = User.select('email').from('users')
   end
 
   def create
     user = User.new(user_params)
-    if user.user_with_same_email(:email.to_s) == false
+    if user.user_with_same_email(user.email) == false
       if user.save
         session[:user_id] = user.id
         redirect_to '/'
       else
         redirect_to '/signup'
       end
+    else
+      redirect_to '/signup'
     end
   end
 
